dc74506e2ee93517f95d43a95b104b5730aa69a0
Abdul Wahab <abdul.wahab@codistan.org>
2024-02-05 23:40:06 +0500

    added routes

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -3,6 +3,7 @@ import homeController from "../controllers/homeController.js";
 import userController from "../controllers/userController.js";
 import authController from "../controllers/authController.js";
 import adminController from "../controllers/adminController.js";
+import storeController from "../controllers/storeController.js";
 
 const router = Router();
 // user route
@@ -11,17 +12,13 @@ const regUser = router.post("/register",userController.register);
 
 const logUser = router.post("/login",userController.login);
 
+
+
 // admin route
 const logadmin = router.post("/admin/login",adminController.login);
 // store route
-const loginStore = router.get("/store/login",(req,res)=>{
-    console.log("Request received on login")
-
-    res.json({
-        "status":200,
-        "Message":"Request Received"
-    })
-})
+const loginStore = router.post("/store/login",storeController.login);
+const regStore = router.post("/store/register",storeController.register);
 // admin route
 
 export {
