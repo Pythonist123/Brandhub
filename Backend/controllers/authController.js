3d7a5eb2e63f093da3427a6f95ae9d5d9af62080
Abdul Wahab <abdul.wahab@codistan.org>
2024-02-06 21:06:25 +0500

    check roles

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,29 +1,44 @@
+// authController.js
+
 import passport from "../middleware/passport.js";
 
 const authController = {
-
-    async authenticate(req,res,next){
-        console.log("Auth middleware");
-        console.log('Request Headers:', req.headers);
-console.log('Token:', req.headers.authorization);
-
+    async authenticate(req, res, next) {
         passport.authenticate('jwt', { session: false }, (err, user) => {
-            console.log(err);
-            console.log(user);
-          if (err || !user) {
-            const error = {
-              status: 401,
-              message: 'Unauthorized',
-            };
-            return res.status(401).json(error);
-          }
-          req.user = user;
-          return next();
+            if (err || !user) {
+                const error = {
+                    status: 401,
+                    message: 'Unauthorized',
+                };
+                return res.status(401).json(error);
+            }
+
+            req.user = user;
+            return next();
         })(req, res, next);
-    }
-}
+    },
 
-    
+    async isAdmin(req, res, next) {
+        if (req.user.role !== 'admin') {
+            const error = {
+                status: 403,
+                message: 'Forbidden. Only admins can access this route.',
+            };
+            return res.status(403).json(error);
+        }
+        next();
+    },
 
+    async isStore(req, res, next) {
+        if (req.user.role !== 'store') {
+            const error = {
+                status: 403,
+                message: 'Forbidden. Only stores can access this route.',
+            };
+            return res.status(403).json(error);
+        }
+        next();
+    },
+};
 
 export default authController;
