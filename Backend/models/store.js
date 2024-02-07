4498d4bcc418dfee95e8344e8e073be950c97994
Abdul Wahab <abdul.wahab@codistan.org>
2024-02-07 19:36:38 +0500

    add category field

--- Backend/models/store.js
+++ Backend/models/store.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const storeSchema = new mongoose.Schema({
+    name:{ type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, default: 'store' }, // Set default value to 'store'
