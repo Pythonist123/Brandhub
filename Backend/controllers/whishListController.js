e46efd44c7c3e2ca2f4c3f3585473248f2e6cc67
Abdul Wahab <abdul.wahab@codistan.org>
2024-02-13 06:41:44 +0500

    created whishlistcontroller

--- Backend/controllers/whishListController.js
+++ Backend/controllers/whishListController.js
@@ -1,5 +1,34 @@
-const whishListController = {
+import User from '../models/user.js';
+import Product from '../models/product.js';
+import Whishlist from '../models/whishlist.js';
 
+const whishListController = {
+// add to whishlist
+    async addToWhishList(req, res, next) {
+        const { productId, userId } = req.body;
+        try {
+            const user = await User.findById(userId);
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            const product = await Product.findById(productId);
+            if (!product) {
+                return res.status(404).json({ message: 'Product not found' });
+            }
+            const whishlist = await Whishlist.findOne({ product: productId, user: userId });
+            if (whishlist) {
+                return res.status(400).json({ message: 'Product already in whishlist' });
+            }
+            const newWhishlist = new Whishlist({
+                product: productId,
+                user: userId
+            });
+            await newWhishlist.save();
+            res.status(200).json({ message: 'Product added to whishlist successfully' });
+        } catch (error) {
+            next(error);
+        }
+    }
 }
 
 export default whishListController;
